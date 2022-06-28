01bde336bee5595a68dc5eb2132e26a162bc97ca
Kyle <kylejmbaylis@gmail.com>
2022-06-28 15:51:54 -0400

    added tests for about page

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {render, cleanup} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+// won't be leftover memory data that could give false results
+afterEach(cleanup);
+
+// declare component being tested
+describe('About component', () => {
+    // First Test
+    it('renders', () => {
+        render(<About />);
+    });
+
+    // Second Test
+    it('matches snapshot DOM node structure', () => {
+        // returns snapshot of About component
+        const {asFragment} = render(<About />);
+
+        // tests/compares if expected and actual outcomes match
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
